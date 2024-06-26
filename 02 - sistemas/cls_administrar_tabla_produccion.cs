3e164535579a285a0f1c6ee02a195ca75f88a7ab
EzequielKen <eze-ken-@hotmail.es>
2024-06-26 06:03:06 -0300

    .

--- 02 - sistemas/cls_administrar_tabla_produccion.cs
+++ 02 - sistemas/cls_administrar_tabla_produccion.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using modulos;
+using paginaWeb;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,41 @@ namespace _02___sistemas
 {
     public class cls_administrar_tabla_produccion
     {
+        public cls_administrar_tabla_produccion(DataTable usuario_BD)
+        {
+            usuarioBD = usuario_BD;
+            servidor = usuarioBD.Rows[0]["servidor"].ToString();
+            puerto = usuarioBD.Rows[0]["puerto"].ToString();
+            usuario_dato = usuarioBD.Rows[0]["usuario_BD"].ToString();
+            usuario_dato = usuarioBD.Rows[0]["usuario_BD"].ToString();
+            contraseña_BD = usuarioBD.Rows[0]["contraseña_BD"].ToString();
+            if ("1" == ConfigurationManager.AppSettings["produccion"])
+            {
+                base_de_datos = ConfigurationManager.AppSettings["base_de_datos"];
+            }
+            else
+            {
+                base_de_datos = ConfigurationManager.AppSettings["base_de_datos_desarrollo"];
+            }
+            consultas = new cls_consultas_Mysql(servidor, puerto, usuario_dato, contraseña_BD, base_de_datos);
+        }
+
+        #region atributos
+        cls_consultas_Mysql consultas;
+        cls_funciones funciones = new cls_funciones();
+        DataTable usuarioBD;
+        string servidor, puerto, usuario_dato, contraseña_BD, base_de_datos;
+
+        DataTable insumos_fabrica;
+        DataTable productos_terminados;
+        DataTable resumen;
+        #endregion
+
+        #region consultas
+        private void consultar_insumos_fabrica()
+        {
+
+        }
+        #endregion
     }
 }
