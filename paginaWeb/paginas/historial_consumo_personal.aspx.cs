20b1ff596116fd6f99f4dcdbe9b5ade5fa59e1e7
EzequielKen <eze-ken-@hotmail.es>
2024-07-31 18:50:57 -0300

    .

--- paginaWeb/paginas/historial_consumo_personal.aspx.cs
+++ paginaWeb/paginas/historial_consumo_personal.aspx.cs
@@ -18,17 +18,19 @@ namespace paginaWeb.paginas
         cls_historial_consumo_personal historial_consumo;
         cls_funciones funciones = new cls_funciones();
         DataTable usuariosBD;
+        DataTable sucursal;
 
         #endregion
         protected void Page_Load(object sender, EventArgs e)
         {
             usuariosBD = (DataTable)Session["usuariosBD"];
+            sucursal = (DataTable)Session["sucursal"];
             historial_consumo = new cls_historial_consumo_personal(usuariosBD);
         }
 
         protected void calendario_SelectionChanged(object sender, EventArgs e)
         {
-            gridview_consumo.DataSource = historial_consumo.get_consumo(usuariosBD.Rows[0]["id"].ToString(), calendario.SelectedDate);
+            gridview_consumo.DataSource = historial_consumo.get_consumo(sucursal.Rows[0]["id"].ToString(), calendario.SelectedDate);
             gridview_consumo.DataBind();
         }
     }
